84fa62a83869233622e378256483b53934cf5fa0
brunoodaraujo <brunoodaraujo@gmail.com>
2024-04-15 21:44:58 -0300

    Entrega do desafio logica DIO

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("olá")
+//informações do heroi
+let nomeHeroi = "Bruno"
+let xpHeroi = 1100
+let nivel;
+
+//Definir o nível
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+console.log("O nome do heroi é " + nomeHeroi + " e está no nível " + nivel);
